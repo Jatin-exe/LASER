f0a7c57f1e59bb156a5c32348cd635b2948b06e9
Jatin.exe <68765596+Jatin-exe@users.noreply.github.com>
2026-03-03 16:55:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-_term() {
-    echo "Caught SIGTERM signal. Logging out and cleaning up."
-    trap - TERM
-    kill -TERM $TAILSCALE_DAEMON_PID
-    wait $TAILSCALE_DAEMON_PID
-}
+set -euo pipefail
 
-trap _term TERM
+echo $PUBLIC_KEY >/root/.ssh/authorized_keys
+chmod 644 /root/.ssh/authorized_keys
 
-/workdir/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
-TAILSCALE_DAEMON_PID=$!
-/workdir/tailscale up --ssh --authkey=${TAILSCALE_AUTHKEY} --hostname=${NODE_NAME:-tt-on-koyeb}
-
-wait
+/usr/sbin/sshd -D
